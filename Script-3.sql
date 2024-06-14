13dde219e4f27b2e6b2c9f5b895a6f8379cba290
Chevalier <rus.lawful@yandex.ru>
2024-06-14 20:45:16 +0300

    initial commit

---
+++ Script-3.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS Bands (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Genre (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(30) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS GenreBand (
+	Genre_id INTEGER REFERENCES Genre(id),
+	Band_id INTEGER REFERENCES Bands(id),
+	CONSTRAINT pk_GenreBand PRIMARY KEY (Genre_id, Band_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Album (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) unique NOT NULL,
+	year_of INTEGER check(year_of>1900)
+);
+
+
+CREATE TABLE IF NOT EXISTS BandAlbum (
+	Album_id INTEGER REFERENCES Album(id),
+	Band_id INTEGER REFERENCES Bands(id),
+	CONSTRAINT pk_BandAlbum PRIMARY KEY (Album_id, Band_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Songs (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	duration INTEGER,
+	Album_id INTEGER REFERENCES Album(id)
+);
+
+CREATE TABLE IF NOT EXISTS CollectionOfSongs (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) unique NOT NULL,
+	year_of INTEGER check(year_of>1900)
+);
+
+CREATE TABLE IF NOT EXISTS SongCollection (
+	Song_id INTEGER REFERENCES Songs(id),
+	CollectionOfSongs_id INTEGER REFERENCES CollectionOfSongs(id),
+	CONSTRAINT pk_SongCollection PRIMARY KEY (Song_id, CollectionOfSongs_id)
+);
